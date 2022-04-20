4ff57ac4baaf1d007210e77f80517290db294a07
Eric Wilfer <ericwilfer789@gmail.com>
2022-04-20 12:59:53 +0530

    feedback now really works

--- main/urls.py
+++ main/urls.py
@@ -3,9 +3,12 @@ from . import views
 from django.conf.urls.static import static
 
 # spell urlpatterns in small letters
+app_name = 'main'
 urlpatterns = [
     path('', views.index),
     path('services.html', views.services),
     path('about.html', views.about),
     path('contact.html', views.contact),
+    path('form/feedback_form.html', views.feedback_form),
+    path('form/thanks.html',views.feedback_form)
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import FeedbackForm
+
 # Create your views here.
 def index(request):
     context = {}
@@ -17,3 +19,16 @@ def about(request):
 def contact(request):
     context = {}
     return render(request, "contact.html")
+
+
+def feedback_form(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return render(request, 'form/thanks.html')
+    else:
+        form = FeedbackForm()
+    context = {'form' : form}
+    return render(request, 'form/feedback_form.html', {'form': form})
