1b706d75c1fb109d3ebf9746e6372e0f66922d1a
ERICWILFER <ericwilfer789@gmail.com>
2022-05-09 18:01:20 +0530

    somehow linked chatbot to web page

--- main/views.py
+++ main/views.py
@@ -1,34 +1,49 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import FeedbackForm
+from . import modulechatbot
 
 # Create your views here.
+
+
 def index(request):
     context = {}
     return render(request, "index.html")
+
 
 def services(request):
     context = {}
     return render(request, "services.html")
 
+
 def about(request):
     context = {}
     return render(request, "about.html")
+
 
 def contact(request):
     context = {}
     return render(request, "contact.html")
 
+
 def gallery(request):
-    context ={}
+    context = {}
     return render(request, "gallery.html")
 
+
 def pythonweb(request):
-    context ={}
+    def chat(msg):
+        # print("Start chatting with the bot (type quit to stop)!")
+        return modulechatbot.response(msg)
+
+    if request.method == 'POST':
+        msg = request.POST.get('input', '')
+        chat(msg)
     return render(request, "pythonweb.html")
 
+
 def feedback_form(request):
-    
+
     if request.method == 'POST':
         form = FeedbackForm(request.POST)
 
@@ -37,17 +52,15 @@ def feedback_form(request):
             return render(request, 'form/thanks.html')
     else:
         form = FeedbackForm()
-    context = {'form' : form}
+    context = {'form': form}
     return render(request, 'form/feedback_form.html', {'form': form})
+
 
 def chatbot(request):
     if request.method == 'POST':
-        msg = request.POST.get('input','')
+        msg = request.POST.get('input', '')
         if msg == 'eric':
             return HttpResponse("eric is alive")
         else:
             return HttpResponse("eric is not alive")
     return render(request, 'chatbot.html')
-    
-    
-    
