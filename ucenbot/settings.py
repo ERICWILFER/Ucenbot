f7e672b7bbe6c8efe8c43c7a65c36d74a1db697e
ERICWILFER <ericwilfer789@gmail.com>
2022-06-01 18:31:33 +0530

    design changed

--- ucenbot/settings.py
+++ ucenbot/settings.py
@@ -133,7 +133,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = [ BASE_DIR, 'static', ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
 
 MEDIA_URL = '/images/'
 
--- ucenbot/urls.py
+++ ucenbot/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
